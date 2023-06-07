bdaf16f23191f88b83aad8f97084d798db7b0403
MD. TANZID HAQUE <tanzid3@gmail.com>
2023-06-07 11:37:26 +0600

    Practice Problem - 1

--- Module-2.5/L_New_Array.cpp
+++ Module-2.5/L_New_Array.cpp
@@ -4,18 +4,11 @@ int main()
 {
     int n; cin>>n;
     vector <int> a(n), b(n), c;
-    
-    for (int i = 0; i < n; i++)
-    {
-        cin>>a[i];
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cin>>b[i];
-    }
+    //Input
+    for (int i = 0; i < n; i++) cin>>a[i];
+    for (int i = 0; i < n; i++) cin>>b[i];
     c.insert(c.begin(),b.begin(),b.end());
     c.insert(c.end(),a.begin(), a.end());
     for (int x:c) cout<<x<<" ";
-    
     return 0;
 }
